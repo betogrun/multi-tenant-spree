42b145ad5c3bccabc92d8009228c8a05d919d16e
Alberto Rocha <betogrun@gmail.com>
2018-04-28 12:19:30 -0300

    Rename app

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Mystore8
+module MultiTenantSpree
   class Application < Rails::Application
     
     config.to_prepare do
